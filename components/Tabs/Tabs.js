bb3538fd176af9cda216310041b0904997225c1f
anthony <stackchain@gmail.com>
2019-04-12 12:16:08 -0400

    still running checks

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,34 +1,28 @@
 class TabLink {
   constructor(tabElement){
 
-/* ********************************************************* 99
-
     // assign this.tabElement to the tabElement DOM reference
     this.tabElement = tabElement;
 
-    ********************************************************* 99 */
-
-    /* *************************************************************** 33
-    
     // Get the `data-tab` value from this.tabElement and store it here
     this.tabData = this.tabElement.dataset.tab; 
 
-    ******************************************************************** 33 */
-
-    /* ********************************************************** 88
     
     // We need to find out if a user clicked 'all' cards or a specific category.  Follow the instructions below to accomplish this task:    
-  
+    
+    this.tabData = `node`;
+
     // Check to see if this.tabData is equal to 'all'
     if(this.tabData === `all`){
       // If `all` is true, select all cards regardless of their data attribute values
-      this.cards = document.querySelectorAll(`.cards`);
+      this.cards = document.querySelectorAll(`.card`);
     } else {
       // else if `all` is false, only select the cards with matching this.tabData values
       this.cards = document.querySelectorAll(`.card[data-tab='${this.tabData}']`);
     }
 
-************************************************************** 88 */
+    console.log(this.cards);
+
 
 
 
@@ -108,4 +102,3 @@ tabs.forEach(function(tab) {
   return new TabLink(tab)
 })
 
-console.log(tabs);
